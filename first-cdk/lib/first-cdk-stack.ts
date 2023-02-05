6dc3c2e1aeaccef152d9ceebb1796b74f5d1126b
meightythree <palkollar1995@gmail.com>
2023-02-05 11:47:44 +0100

    feat: :sparkles: CfnParameter

--- first-cdk/lib/first-cdk-stack.ts
+++ first-cdk/lib/first-cdk-stack.ts
@@ -1,15 +1,23 @@
 import * as cdk from "aws-cdk-lib";
-import { CfnOutput, Duration } from "aws-cdk-lib";
+import { CfnOutput, CfnParameter, Duration } from "aws-cdk-lib";
 import { Bucket } from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class FirstCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const duration = new CfnParameter(this, "duration", {
+      type: "Number",
+      default: 6,
+      minValue: 1,
+      maxValue: 10,
+    });
+
     const myBucket = new Bucket(this, "myBucket", {
-      lifecycleRules: [{ expiration: Duration.days(5) }],
+      lifecycleRules: [
+        { expiration: Duration.days(duration.value as unknown as number) },
+      ],
     });
 
     new CfnOutput(this, "myBucketCfnOutput", {
