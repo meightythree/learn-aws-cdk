f9fee04c703523838ca5c9ac4f03a0cf2068919b
meightythree <palkollar1995@gmail.com>
2023-02-05 12:56:11 +0100

    feat: :sparkles: RestApi

--- space-finder/serverless-backend/infrastructure/SpaceStack.ts
+++ space-finder/serverless-backend/infrastructure/SpaceStack.ts
@@ -6,8 +6,11 @@ import {
   Runtime,
 } from "aws-cdk-lib/aws-lambda";
 import { join } from "path";
+import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
 
 export class SpaceStack extends Stack {
+  private api = new RestApi(this, "SpaceApi");
+
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
 
@@ -16,5 +19,10 @@ export class SpaceStack extends Stack {
       code: Code.fromAsset(join(__dirname, "../services/hello")),
       handler: "hello.main",
     });
+
+    // Hello LambdaIntegration
+    const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+    const helloLambdaResource = this.api.root.addResource("hello");
+    helloLambdaResource.addMethod("GET", helloLambdaIntegration);
   }
 }
