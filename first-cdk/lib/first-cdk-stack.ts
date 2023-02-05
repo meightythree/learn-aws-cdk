6e689f9795bcf22bdfc107bc41f2cf9ee81f8067
meightythree <palkollar1995@gmail.com>
2023-02-05 11:41:31 +0100

    feat: :sparkles: CfnOutput

--- first-cdk/lib/first-cdk-stack.ts
+++ first-cdk/lib/first-cdk-stack.ts
@@ -1,16 +1,19 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { CfnOutput, Duration } from "aws-cdk-lib";
+import { Bucket } from "aws-cdk-lib/aws-s3";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class FirstCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const myBucket = new Bucket(this, "myBucket", {
+      lifecycleRules: [{ expiration: Duration.days(5) }],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'FirstCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new CfnOutput(this, "myBucketCfnOutput", {
+      value: myBucket.bucketName,
+    });
   }
 }
